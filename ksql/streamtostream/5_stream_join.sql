c4f9119720f81866342880b0edc99f7564d68894
Jeremy Heng <hengweyseing53@gmail.com>
2025-05-17 17:31:51 +0800

    [DONE] test stream2stream -> window

--- ksql/streamtostream/5_stream_join.sql
+++ ksql/streamtostream/5_stream_join.sql
@@ -5,9 +5,10 @@ SELECT
     o.product AS product,
     o.buyer_id AS buyer_id,
     o.amount AS amount,
-    o.create_date AS create_date,
-    b.name AS buyer_name
+    o.create_date AS o_create_date,
+    b.name AS buyer_name,
+    b.create_date AS b_create_date
 FROM streamtostream_stream_order_intake o
-JOIN streamtostream_stream_buyer_intake b
+LEFT JOIN streamtostream_stream_buyer_intake b
 WITHIN (5 MINUTES, 10 MINUTES)
 ON o.buyer_id = b.id;
